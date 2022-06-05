398c572a17ac5bb7d19c00b0d271fa6883a654b2
DarkDeveloper <goldeagle00110011@outlook.com>
2022-06-05 22:58:29 +0430

    updated delay

--- src/main/java/org/example/customjavareactapp/service/CustomService.java
+++ src/main/java/org/example/customjavareactapp/service/CustomService.java
@@ -17,7 +17,7 @@ public class CustomService {
     private final DataRepo dataRepo;
 
     public Mono<Data> getData() {
-        return dataRepo.findAll().elementAt(0).delayElement(Duration.ofSeconds(1));
+        return dataRepo.findAll().elementAt(0).delayElement(Duration.ofMillis(100));
     }
 
     public Mono<Data> postData(DataDto dataDto) {
@@ -25,16 +25,7 @@ public class CustomService {
                 .data(dataDto.data())
                 .requestCameTime(System.currentTimeMillis())
                 .build();
-        return dataRepo.save(data).delayElement(Duration.ofMillis(100));
+        return dataRepo.save(data).delayElement(Duration.ofSeconds(1));
     }
 
-//    @PostConstruct
-//    void saveFirstData(){
-//        var data = Data.builder()
-//                .id(UUID.fromString("cd01675c-77da-4cfd-af32-36dbb7873782"))
-//                .data("data")
-//                .requestCameTime(System.currentTimeMillis())
-//                .build();
-//        dataRepo.save(data);
-//    }
 }
